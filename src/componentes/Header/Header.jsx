93ce4c0c3e1e9b3e006dae89ebc5f4a8bf87e493
ANDRESSA CHAGAS <andressa.chagas79@gmail.com>
2023-03-27 13:58:06 -0300

    Styles Header

--- src/componentes/Header/Header.jsx
+++ src/componentes/Header/Header.jsx
@@ -4,18 +4,19 @@ import imgNav from "../../assets/Nav.jpg";
 const Header = () => {
   return (
     <section className={style.section}>
-      <div className={style.divNav}>
-        <img className={style.imgNav} src={imgNav} />
-        <img className={style.imgNav2} src={imgNav} />
-        <img className={style.imgNav3} src={imgNav} />
-        <title className={style.title}>Casa das plantas</title>
-        <nav className={style.nav}>
+      <nav className={style.nav}>
           <ul className={style.ul}>
             <li className={style.filtro}>Home</li>
             <li className={style.filtro}>Plantas Internas</li>
             <li className={style.filtro}>Contatos</li>
           </ul>
         </nav>
+      <div className={style.divNav}>
+        <img className={style.imgNav} src={imgNav} />
+        <img className={style.imgNav2} src={imgNav} />
+        <img className={style.imgNav3} src={imgNav} />
+        <title className={style.title}>Casa das plantas</title>
+
       </div>
     </section>
   );
